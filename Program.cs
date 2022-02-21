36f91dc0a0dc7dd7c93554a294ebb0e65fed8110
Illia Katrechko <XKATI002@studenti.czu.cz>
2022-02-21 18:16:45 +0100

    Init commit

--- Program.cs
+++ Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using Figgle;
 
-namespace ConsoleApp2
+namespace Diary_1._0
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(
+            FiggleFonts.Slant.Render("Diary v 1.0"));
+            Console.WriteLine("1. Create note [c]");
+            Console.WriteLine("2. Edit note [e]");
+            Console.WriteLine("3. Delete [d]");
+            Console.WriteLine("4. Exit [E]");
+            var answer = Console.ReadKey().KeyChar;
+            switch (answer)
+            {
+                case 'c':
+                    break;
+                case 'e':
+                    break;
+                case 'd':
+                    break;
+                case 'E':
+                    break;
+            }
+            
+          
         }
     }
 }
