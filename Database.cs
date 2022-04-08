4c6b4f5b42f70e0e487a16f5736484ba6b04ebb8
kid48 <21201234a@gmail.com>
2022-04-08 03:45:00 +0200

    next try

---
+++ Database.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Data.SQLite;
+using System.IO;
+
+namespace Diary_1._0
+
+{
+    public class Database
+    {
+        public SQLiteConnection myConnection;
+
+        public Database()
+        {
+            myConnection = new SQLiteConnection("Data Source=main.db");
+            if (!File.Exists("./Notes.sqlite3")){}
+            {
+                SQLiteConnection.CreateFile("Notes.sqlite3");
+            }
+        }
+
+        public void OpenConnection()
+        {
+            if (myConnection.State != System.Data.ConnectionState.Open)
+            {
+                myConnection.Open();
+            }
+        }
+
+        public void CloseConnection()
+        {
+            if (myConnection.State != System.Data.ConnectionState.Closed)
+            {
+                myConnection.Close();
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,83 +2,46 @@
 using Figgle;
 using System.Data.SQLite;
 
+
 namespace Diary_1._0
 {
     class Program
     {
-        static SQLiteConnection connection;
-        static SQLiteCommand command;
-
-        static public bool Connect(string fileName)
-        {
-            try
-            {
-                connection = new SQLiteConnection("Data Source=" + fileName + ";Version=3; FailIfMissing=False");
-                connection.Open();
-                return true;
-            }
-            catch (SQLiteException ex)
-            {
-                Console.WriteLine($"Ошибка доступа к базе данных. Исключение: {ex.Message}");
-                return false;
-            }
-        }
-
- 
-
- 
 
         static void Main(string[] args)
         {
-            if (Connect("main.db"))
-            {
-                Console.WriteLine("Connected");
-            }
-
+            Database databaseObject = new Database();
             Console.WriteLine(
                 FiggleFonts.Slant.Render("Diary v 1.0"));
             Console.WriteLine("1. Create note [1]");
-            Console.WriteLine("2. Edit note [2]");
+            Console.WriteLine("2. Display notes [2]");
             Console.WriteLine("3. Delete [d]");
             Console.WriteLine("4. Exit [E]");
             string answer = Console.ReadLine();
            
-            if (int.Parse(answer) == 1)
+            if (int.Parse(answer) == 1 && answer != null)
             {
+                Console.Write(" Введите ноавую запись:  ");
                 string note = Console.ReadLine();
-                string request = GenerateRequest(note);
-                Console.WriteLine(value: request);
+                DateTime thisDay = DateTime.Now;
+                string query = "INSERT INTO notes (note, date) VALUES (@note, @date);";
+                SQLiteCommand command = new SQLiteCommand(query, databaseObject.myConnection);
+                databaseObject.OpenConnection();
+                command.Parameters.AddWithValue("@note", note);
+                command.Parameters.AddWithValue("@date", thisDay);
+                command.ExecuteNonQuery();
+                databaseObject.CloseConnection();
+                Console.WriteLine("Запись создана");
+                
             }
             else if (int.Parse(answer) == 2)
             {
-                fact(8);
+
             }else
             {
                 
             }
         }
-        static string GenerateRequest(string note)
-        {
-            DateTime thisDay = DateTime.Today;
-            return $"INSERT INTO notes (note, time) VALUES ('{note}', '{thisDay}');";
-        }
-        static int fact(int cislo)
-        {
-            if (cislo > 1)
-            {
-                int result = 1;
-                int i = 1;
-                while (i <= cislo)
-                {
-                    result = result * i;
-                    i++;
-                    
-                }
-                return cislo;
-            }else
-            {
-                return 1;
-            }
-        }
+     
     }
 }
